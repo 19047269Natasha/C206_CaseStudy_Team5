8aee50926d401d1fc418bfb6024acc7c6cc54c96
PinkChan <68525934+PinkChan@users.noreply.github.com>
2020-08-24 16:42:37 +0800

    Update CourseCategory.java

--- C206_CaseStudy/src/CourseCategory.java
+++ C206_CaseStudy/src/CourseCategory.java
@@ -1,14 +1,42 @@
-
 public class CourseCategory {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		CourseCategory cat = new CourseCategory();
+		cat.start();
 
 	}
 
 	public void start() {
 		// TODO Auto-generated method stub
+		int option = 0;
+
+		while (option != 6) {
+
+			CourseDB.showCourseMenu();
+			System.out.println("");
+			option = Helper.readInt("Enter option > ");
+
+			if (option == 1) {
+				System.out.println(CourseCatDB.viewAllCourseCategory());
+			} else if (option == 2) {
+				addCourseCategory();
+			} else if (option == 3) {
+				updateCourseCategory();
+			} else if (option == 4) {
+				System.out.println("Goodbye!");
+			} else {
+				System.out.println("Invalid Option");
+			}
+
+			System.out.println("");
+		}
 		
+	}
+	public void addCourseCategory() {
+		String CourseCategoryName = Helper.readString("Enter Course Category Name > ");
+		String CourseCatetoryDescription = Helper.readString("Enter Course Category Description > ");
 	}
 
 }
