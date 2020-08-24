be094dbb6e8decef031a7825323c4ac3649ad492
19047269 <19047269@DESKTOP-V6FB4NK>
2020-08-24 11:59:06 +0800

    Edited

--- C206_CaseStudy/src/CourseMain.java
+++ C206_CaseStudy/src/CourseMain.java
@@ -1,9 +1,70 @@
 
 public class CourseMain {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	private String courseCode;
+	private String courseTitle;
+	private String categoryName;
+	private String courseDescription;
+	private int courseDuration;
+	private String prerequisiteCourse;
+	
+	public CourseMain(String courseCode, String courseTitle, String categoryName, String courseDescription, int courseDuration, String prerequisiteCourse) {
+		
+		this.courseCode = courseCode;
+		this.courseTitle = courseTitle;
+		this.categoryName = categoryName;
+		this.courseDescription = courseDescription;
+		this.courseDuration = courseDuration;
+		this.prerequisiteCourse = prerequisiteCourse;
 	}
+
+	public String getCourseCode() {
+		return courseCode;
+	}
+
+	public void setCourseCode(String courseCode) {
+		this.courseCode = courseCode;
+	}
+
+	public String getCourseTitle() {
+		return courseTitle;
+	}
+
+	public void setCourseTitle(String courseTitle) {
+		this.courseTitle = courseTitle;
+	}
+
+	public String getCategoryName() {
+		return categoryName;
+	}
+
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+
+	public String getCourseDescription() {
+		return courseDescription;
+	}
+
+	public void setCourseDescription(String courseDescription) {
+		this.courseDescription = courseDescription;
+	}
+
+	public int getCourseDuration() {
+		return courseDuration;
+	}
+
+	public void setCourseDuration(int courseDuration) {
+		this.courseDuration = courseDuration;
+	}
+
+	public String getPrerequisiteCourse() {
+		return prerequisiteCourse;
+	}
+
+	public void setPrerequisiteCourse(String prerequisiteCourse) {
+		this.prerequisiteCourse = prerequisiteCourse;
+	}
+	
 
 }
