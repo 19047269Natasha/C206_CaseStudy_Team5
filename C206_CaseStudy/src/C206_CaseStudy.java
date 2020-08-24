9b4c3494dae53eb3accd4ce4617f0f8d99daf0d6
19047269 <19047269@DESKTOP-V6FB4NK>
2020-08-24 10:53:08 +0800

    Edited Case Study

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -3,6 +3,13 @@ public class C206_CaseStudy {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		int option = 0;
+		
+		while(option != 6) {
+			System.out.println("MAIN MENU");
+			Helper.line(30, "-");
+		}
 
 	}
 
