30f90116bf8d0af6fa67d1144d64bf25d93f7249
EstellaSeah19025237 <68535196+EstellaSeah19025237@users.noreply.github.com>
2020-08-27 13:51:42 +0800

    Update CourseSchedule.java

--- C206_CaseStudy/src/CourseSchedule.java
+++ C206_CaseStudy/src/CourseSchedule.java
@@ -9,6 +9,54 @@ public class CourseSchedule {
 	public void start() {
 		// TODO Auto-generated method stub
 		
+		/**
+ * @author 19025237
+ *
+ */
+        public class C206_CaseStudy {
+
+        public void addcourseschedule() {
+	    int scheduleID = Helper.readInt("Enter Course Schedule id");
+	    double price = Helper.readInt("Enter Course price");
+	    String startdatetime = Helper.readString("Enter start date and time (DD/MM/YYYY 00:00)");
+	    String enddatetime = Helper.readString("Enter start date and time (DD/MM/YYYY 00:00)");
+	    String location = Helper.readString("Enter course location");
+	
+	    for (courseschedule schedule:schedulelist) {
+		if (scheduleID == schedule.getID()) {
+		    System.out.println("This course schedule already exists.");
+		    run.addcourseschedule();
+	        }    
+	    }
+	
+	    courseschedule newschedule = new courseschedule (scheduleID, price, startdatetime, enddatetime, location);
+	    schedulelist.add(newschedule);
+    }
+
+    public void viewcourseschedule() {
+	    String output = String.format("%s %5s %10s %5s, "Schedule ID", "Price", "Start Date Time", "End Date Time", "Location");"
+	
+	    for(courseschedule schedule:schedulelist) {
+		    output += String.format("\n %d %5f %10s %s", schedule.getID(), schedulegetPrice(), schedule.getstartdatetime(), schedule.getenddatetime, schedule.getlocation());
+	    }
+	    System.out.println(output);
+	
+    }
+    public void deletecourseschedule() {
+    	viewcourseschedule();
+	int deleteschedule = Helper.readInt("ID of course schedule to delete");
+	
+	for (courseschedule schedule:schedulelist) {
+		if(deleteschedule == schedule.getID());
+		    schedulelist.remove(schedule);
+	    }else {
+	    	System.out.println("ID doesen't exist");
+	    }	
+	}
+	    
+}
+
+		
 	}
 
 }
