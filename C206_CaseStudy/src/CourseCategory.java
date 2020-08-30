57f79604dfc8d99468d1aa2f29f3d387a1e77e38
PinkChan <68525934+PinkChan@users.noreply.github.com>
2020-08-30 20:39:57 +0800

    Update CourseCategory.java

--- C206_CaseStudy/src/CourseCategory.java
+++ C206_CaseStudy/src/CourseCategory.java
@@ -1,14 +1,48 @@
 
 public class CourseCategory {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+			private String ID;
+			private String name;
+			private String description;
+	
+			public CourseCategory(String iD, String name, String Description) {
+				this.name = name;
+				this.ID = ID;
+				
+			}
+
+			public String getID() {
+				return ID;
+			}
+
+			public void setID(String iD) {
+				ID = iD;
+			}
+
+			public String getName() {
+				return name;
+			}
+
+			public void setName(String name) {
+				this.name = name;
+			}
+			
+			public String getDescription() {
+				return description;
+			}
+			
+			public void setDescription(String description) {
+				this.description = description;
+			}
 	}
 
-	public void start() {
-		// TODO Auto-generated method stub
+			
+	
+
 		
-	}
 
-}
+
+	
+
+	
